a0b0841dcc6b24ac694e2a8e66eccb972c0f9d63
James <jmelonid@lsoc.org>
2016-02-08 12:06:01 -0600

    done

--- array_of_challenges.rb
+++ array_of_challenges.rb
@@ -9,17 +9,13 @@ puts James_array
 	#That first array, star_students and this array, brilliant_students, should be one big array!
 		#combine this array with the one above
 		#Then sort the final array in alphabetic order
-
 	brilliant_students = ["James", "Annika", "Ned", "Alden", "Daniel", "Ben", "Luke", "Jackson"]
 all_students = brilliant_students + star_students 
 all_students.sort_by! {|name|name.downcase}
 puts all_students
-	
 	#BONUS
 		#Iterate through the final student array, printing each student with an adjective
 			#ex:   Dynamic Dean   /n  Witty Andrew  /n  Diplomatic Luke
-
-
 			adjectives = ["adventurous", "ambitious", "brave", "bright", "charming", "courageous", "creative", "determined", "dynamic", "diplomatic", "enthusiastic", "helpful", "honest", "humorous", "inventive", "persistent", "powerful", "reliable", "resourceful", "sincere", "thoughtful", "witty"]
 all_students.each do |student|
 	puts adjectives[rand(adjectives.length-1)].capitalize + " " + student
